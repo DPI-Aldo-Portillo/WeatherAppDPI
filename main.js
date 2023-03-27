afdab93030ea906e3949ac892f08d57dbce600fa
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-03-26 20:12:55 -0500

    Add event listener to form

--- main.js
+++ main.js
@@ -1,40 +1,45 @@
-console.log("hello world")
-
-
 const key = "6b90e7ed6704a9984fc25f85e2580025"
-const query = "chicago"
 
 
 
 const url = new URL("http://api.openweathermap.org/data/2.5/weather?")
-url.searchParams.set('q',query)
-url.searchParams.set('units','imperial')
-url.searchParams.set('id', '524901')
-url.searchParams.set('appid',key)
 
-
-const cityNameHTML = document.getElementById("cityName")
+const cityNameHTML = document.getElementById("city-name")
 const humidityHTML = document.getElementById("humidity")
 const tempHTML = document.getElementById("temp")
 const tempMinHTML = document.getElementById("temp-min")
 const tempMaxHTML = document.getElementById("temp-max")
 const feelsLikeHTML = document.getElementById("feels-like")
 
-async function callWeather(){
+const form = document.getElementById("city-form")
+
+async function callWeather(query){
+
+    url.searchParams.set('q',query)
+    url.searchParams.set('units','imperial')
+    url.searchParams.set('id', '524901')
+    url.searchParams.set('appid',key)
+
     const res = await fetch(url)
     const data = await res.json()
     console.log(data.main)
 
     const {temp, feels_like, temp_min, temp_max, pressure, humidity} = data.main
+
+    cityNameHTML.textContent = data.name
     tempHTML.textContent = temp
     feelsLikeHTML.textContent = feels_like
     tempMinHTML.textContent = temp_min
     tempMaxHTML.textContent = temp_max
     humidityHTML.textContent = humidity
     
+    console.log(data)
     return data
 }
 
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    const query = e.target[0].value
+    callWeather(query)
+})
 
-
-console.log(callWeather())
